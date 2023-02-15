641b021dcb92c6775ef23c16f66837528e3dc646
chetverninart <chetverninart@gmail.com>
2023-02-15 21:10:44 +0300

    Initial commit

---
+++ src/main/java/ru/pudge/pudge/PudgeApplication.java
@@ -0,0 +1,13 @@
+package ru.pudge.pudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PudgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pudge/pudge/PudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pudge.pudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
